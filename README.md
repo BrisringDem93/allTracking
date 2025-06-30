bcfe3dc68b1fc62df653b3ecef646e4d2f629f72
BrisringDem93 <deminicis09@gmail.com>
2025-06-30 19:03:15 +0200

    Add Facebook event tracker plugin

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # allTracking
-Tracking your site. 
+
+Un semplice plugin WordPress per tracciare gli eventi e inviarli alla Facebook Graph API.
+
+## Installazione
+
+1. Copia la cartella `fb-event-tracker` all'interno della directory `wp-content/plugins` del tuo sito WordPress.
+2. Attiva il plugin dal pannello di amministrazione di WordPress.
+3. Dal menu "FB Event Tracker" inserisci il token API e l'ID del pixel forniti da Facebook.
+
+Il plugin, una volta configurato, invierà automaticamente un evento `PageView` ad ogni caricamento di pagina.
---
+++ fb-event-tracker/fb-event-tracker.php
@@ -0,0 +1,108 @@
+<?php
+/**
+ * Plugin Name: FB Event Tracker
+ * Description: Traccia gli eventi del sito e li invia a Facebook Graph API.
+ * Version: 0.1
+ * Author: Your Name
+ */
+
+if (!defined('ABSPATH')) {
+    exit; // Exit if accessed directly
+}
+
+function fb_event_tracker_add_menu() {
+    add_menu_page(
+        'FB Event Tracker',
+        'FB Event Tracker',
+        'manage_options',
+        'fb-event-tracker',
+        'fb_event_tracker_settings_page'
+    );
+}
+add_action('admin_menu', 'fb_event_tracker_add_menu');
+
+function fb_event_tracker_settings_page() {
+    ?>
+    <div class="wrap">
+        <h1>FB Event Tracker</h1>
+        <form method="post" action="options.php">
+            <?php
+            settings_fields('fb_event_tracker_settings');
+            do_settings_sections('fb_event_tracker');
+            submit_button();
+            ?>
+        </form>
+    </div>
+    <?php
+}
+
+function fb_event_tracker_settings_init() {
+    register_setting('fb_event_tracker_settings', 'fb_event_tracker_token');
+    register_setting('fb_event_tracker_settings', 'fb_event_tracker_pixel_id');
+
+    add_settings_section(
+        'fb_event_tracker_section',
+        __('Settings', 'fb-event-tracker'),
+        '__return_false',
+        'fb_event_tracker'
+    );
+
+    add_settings_field(
+        'fb_event_tracker_token',
+        __('API Token', 'fb-event-tracker'),
+        'fb_event_tracker_token_render',
+        'fb_event_tracker',
+        'fb_event_tracker_section'
+    );
+
+    add_settings_field(
+        'fb_event_tracker_pixel_id',
+        __('Pixel ID', 'fb-event-tracker'),
+        'fb_event_tracker_pixel_id_render',
+        'fb_event_tracker',
+        'fb_event_tracker_section'
+    );
+}
+add_action('admin_init', 'fb_event_tracker_settings_init');
+
+function fb_event_tracker_token_render() {
+    $value = esc_attr(get_option('fb_event_tracker_token', ''));
+    echo '<input type="text" name="fb_event_tracker_token" value="' . $value . '" class="regular-text">';
+}
+
+function fb_event_tracker_pixel_id_render() {
+    $value = esc_attr(get_option('fb_event_tracker_pixel_id', ''));
+    echo '<input type="text" name="fb_event_tracker_pixel_id" value="' . $value . '" class="regular-text">';
+}
+
+function fb_event_tracker_send_event($event_name, $event_data = array()) {
+    $token = get_option('fb_event_tracker_token');
+    $pixel_id = get_option('fb_event_tracker_pixel_id');
+
+    if (!$token || !$pixel_id) {
+        return;
+    }
+
+    $url = sprintf('https://graph.facebook.com/v15.0/%s/events?access_token=%s', $pixel_id, $token);
+
+    $payload = array(
+        'data' => array(
+            array_merge(array(
+                'event_name' => $event_name,
+                'event_time' => time(),
+                'event_source_url' => home_url($_SERVER['REQUEST_URI']),
+                'action_source' => 'website',
+            ), $event_data)
+        )
+    );
+
+    wp_remote_post($url, array(
+        'headers' => array('Content-Type' => 'application/json'),
+        'body'    => wp_json_encode($payload),
+    ));
+}
+
+function fb_event_tracker_page_view() {
+    fb_event_tracker_send_event('PageView');
+}
+add_action('wp_footer', 'fb_event_tracker_page_view');
