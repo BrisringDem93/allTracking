24a7ed75fc915b6d0c0c389e58fb9226ed93886a
Brisring93 <deminicis09@gmail.com>
2026-07-10 18:39:16 +0200

    Update plugin.php

--- plugin.php
+++ plugin.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: Quick Tracking Integration
  * Description: Inserisce automaticamente Facebook Pixel, Google Analytics 4 e Google Tag Manager con una semplice configurazione.
- * Version: 0.7.1
+ * Version: 0.7.2
  * Author: Francesco de Minicis
  */
 
